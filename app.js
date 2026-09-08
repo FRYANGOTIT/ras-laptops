1ec4b5354725a9b1fb4cab1e6cd4c1f4a22675dd
FRYANGOTIT <ryanfarhat0@gmail.com>
2026-09-08 11:53:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ const CONFIG = {
   whatsappDisplay: '+961 76 792 834',
 
   /* >>> PASTE YOUR PUBLISHED GOOGLE SHEET CSV LINK BETWEEN THESE QUOTES <<< */
-  sheetCsvUrl: 'https://docs.google.com/spreadsheets/d/e/PASTE_YOUR_PUBLISHED_SHEET_ID_HERE/pub?gid=0&single=true&output=csv',
+  sheetCsvUrl: 'https://docs.google.com/spreadsheets/d/1DNb3tbtvwbNJ4BFg4YGWaA7Op5xjaq_V0FhOGQxdnc4/edit',
 
   /* Folder that holds the product photos. Keep the trailing slash. */
   imagesPath: 'images/',
